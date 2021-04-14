c615edb6ca32f9d26b02b4a7b89dccadc12070c9
ken3986 <naka_ken8686@yahoo.co.jp>
2021-04-15 02:19:14 +0900

    setHeader

--- src/express.js
+++ src/express.js
@@ -12,38 +12,49 @@ const serverless = require('serverless-http');
 // const bodyParser = require('body-parser');
 // app.use(bodyParser);
 
+// const allowCrossDomain = (req, res, next) => {
+//   res.writeHead(200, {
+//     'Content-Type': 'text/html',
+//     'Access-Control-Allow-Origin': '*',
+//     'Access-Control-Allow-Headers': 'Content-Type',
+//     'Access-Control-Allow-Methods': 'GET, PUT, POST, DELETE'
+//   });
+//   next();
+// }
+
+// app.use(allowCrossDomain);
+const allowCORS = {
+  'Content-Type': 'text/html',
+  'Access-Control-Allow-Origin': '*',
+  'Access-Control-Allow-Headers': 'Content-Type',
+  'Access-Control-Allow-Methods': 'GET, PUT, POST, DELETE'
+}
+
 // ルーティング
 const router = express.Router();
 
   router.get('/', (req, res) => {
-    // res.header('Content-Type', 'application/json; charset=utf-8');
-    // res.send({result: 'express'});
-    // res.header('Access-Control-Allow-Origin', '*');
-    // res.header('Access-Control-Allow-Headers', 'Content-Type');
-    // res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-    // res.status(200);
+    // res.writeHead(200, allowCORS);
+    res.setHeader('Access-Control-Allow-Origin', '*');
+
+    res.write('<h1>Hello from Express.js!!!!!!</h1>');
+    res.end();
+  });
+
+  router.get('/test', (req, res) => {
     // res.writeHead(200, {
-    //   'Content-Type': 'text/html',
+    //   'Content-Type': 'application/json',
     //   'Access-Control-Allow-Origin': '*',
     //   'Access-Control-Allow-Headers': 'Content-Type',
     //   'Access-Control-Allow-Methods': 'GET, PUT, POST, DELETE'
     // });
+    res.header('Access-Control-Allow-Origin', '*');
 
-    // res.send('express');
-    res.writeHead(200, {
-      'Content-Type': 'text/html',
-      'Access-Control-Allow-Origin': '*',
-      'Access-Control-Allow-Headers': 'Content-Type',
-      'Access-Control-Allow-Methods': 'GET, PUT, POST, DELETE'
-    });
-    res.write('<h1>Hello from Express.js!</h1>');
+    res.header('Content-Type', 'application/json; charset=utf-8');
+    res.json({value: "valval"});
     res.end();
-    // res.send('express');
+    // res.send({"value": "val"});
   });
-
-  router.get('/test', (req, res) => {
-    res.send('test');
-  })
 
 app.use('/.netlify/functions/express', router);
 
