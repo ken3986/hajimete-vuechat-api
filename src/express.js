0175801535e7aa89ec13d5e8c753855ea6c30a28
ken3986 <naka_ken8686@yahoo.co.jp>
2021-04-14 21:19:08 +0900

    writeHeadにしてみた

--- src/express.js
+++ src/express.js
@@ -9,17 +9,36 @@ app.use(cors);
 // serverless読み込み
 const serverless = require('serverless-http');
 
+// const bodyParser = require('body-parser');
+// app.use(bodyParser);
+
 // ルーティング
 const router = express.Router();
 
   router.get('/', (req, res) => {
     // res.header('Content-Type', 'application/json; charset=utf-8');
     // res.send({result: 'express'});
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Headers', 'Content-Type');
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-    res.status(200);
-    res.send('express');
+    // res.header('Access-Control-Allow-Origin', '*');
+    // res.header('Access-Control-Allow-Headers', 'Content-Type');
+    // res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    // res.status(200);
+    // res.writeHead(200, {
+    //   'Content-Type': 'text/html',
+    //   'Access-Control-Allow-Origin': '*',
+    //   'Access-Control-Allow-Headers': 'Content-Type',
+    //   'Access-Control-Allow-Methods': 'GET, PUT, POST, DELETE'
+    // });
+
+    // res.send('express');
+    res.writeHead(200, {
+      'Content-Type': 'text/html',
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Headers': 'Content-Type',
+      'Access-Control-Allow-Methods': 'GET, PUT, POST, DELETE'
+    });
+    res.write('<h1>Hello from Express.js!</h1>');
+    res.end();
+    // res.send('express');
   });
 
   router.get('/test', (req, res) => {
