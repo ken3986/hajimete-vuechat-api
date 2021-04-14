52d563a28f3673a77d17c16d9ffa81271ccea062
ken3986 <naka_ken8686@yahoo.co.jp>
2021-04-14 18:32:54 +0900

    CROS許可

--- src/app.js
+++ src/app.js
@@ -2,6 +2,9 @@ module.exports.handler = async() => {
   return {
     statusCode: 200,
     headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Headers': 'Content-Type',
+      'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE',
       'Content-Type': 'text/plain'
     },
     body: 'Hello'
