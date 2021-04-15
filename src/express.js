a7eabcf198b4acf1bd5a06364496e2e70dc0983b
ken3986 <naka_ken8686@yahoo.co.jp>
2021-04-15 12:36:21 +0900

    res.sendに統一

--- src/express.js
+++ src/express.js
@@ -35,10 +35,12 @@ const router = express.Router();
 
   router.get('/', (req, res) => {
     // res.writeHead(200, allowCORS);
-    res.setHeader('Access-Control-Allow-Origin', '*');
+    // res.setHeader('Access-Control-Allow-Origin', '*');
 
-    res.write('<h1>Hello from Express.js!!!!!!</h1>');
-    res.end();
+    // res.write('<h1>Hello from Express.js!!!!!!</h1>');
+    // res.end();
+    res.header('Content-Type', 'text/html');
+    res.send('<h1>default page</h1>');
   });
 
   router.get('/test', (req, res) => {
@@ -48,16 +50,19 @@ const router = express.Router();
     //   'Access-Control-Allow-Headers': 'Content-Type',
     //   'Access-Control-Allow-Methods': 'GET, PUT, POST, DELETE'
     // });
-    res.header('Access-Control-Allow-Origin', '*');
+    // res.header('Access-Control-Allow-Origin', '*');
 
-    res.header('Content-Type', 'application/json; charset=utf-8');
-    res.json({value: "valval"});
-    res.end();
+    // res.header('Content-Type', 'application/json; charset=utf-8');
+    // res.json({value: "valval"});
+    // res.end();
     // res.send({"value": "val"});
+    res.header('Content-Type', 'text/plain');
+    res.send('test1 plain');
   });
 
   router.get('/test2', (req, res) => {
-    res.send('test2');
+    res.header('Content-Type', 'application/json; charset=utf-8')
+    res.send({value: "テスト２"});
   });
 
 app.use('/.netlify/functions/express', router);
