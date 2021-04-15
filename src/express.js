4f4ced949694fbec3a4900f1accd921c434b4888
ken3986 <naka_ken8686@yahoo.co.jp>
2021-04-15 16:36:46 +0900

    netlify.tomlのheader情報削除

--- src/express.js
+++ src/express.js
@@ -9,53 +9,17 @@ app.use(cors());
 // serverless読み込み
 const serverless = require('serverless-http');
 
-// const bodyParser = require('body-parser');
-// app.use(bodyParser);
-
-// const allowCrossDomain = (req, res, next) => {
-//   res.writeHead(200, {
-//     'Content-Type': 'text/html',
-//     'Access-Control-Allow-Origin': '*',
-//     'Access-Control-Allow-Headers': 'Content-Type',
-//     'Access-Control-Allow-Methods': 'GET, PUT, POST, DELETE'
-//   });
-//   next();
-// }
-
-// app.use(allowCrossDomain);
-// const allowCORS = {
-//   'Content-Type': 'text/html',
-//   'Access-Control-Allow-Origin': '*',
-//   'Access-Control-Allow-Headers': 'Content-Type',
-//   'Access-Control-Allow-Methods': 'GET, PUT, POST, DELETE'
-// }
 
 // ルーティング
 const router = express.Router();
 
   router.get('/', (req, res) => {
-    // res.writeHead(200, allowCORS);
-    // res.setHeader('Access-Control-Allow-Origin', '*');
-
-    // res.write('<h1>Hello from Express.js!!!!!!</h1>');
-    // res.end();
     res.header('Content-Type', 'text/html');
     res.send('<h1>default page</h1>');
   });
 
   router.get('/test', (req, res) => {
-    // res.writeHead(200, {
-    //   'Content-Type': 'application/json',
-    //   'Access-Control-Allow-Origin': '*',
-    //   'Access-Control-Allow-Headers': 'Content-Type',
-    //   'Access-Control-Allow-Methods': 'GET, PUT, POST, DELETE'
-    // });
-    // res.header('Access-Control-Allow-Origin', '*');
 
-    // res.header('Content-Type', 'application/json; charset=utf-8');
-    // res.json({value: "valval"});
-    // res.end();
-    // res.send({"value": "val"});
     res.header('Content-Type', 'text/plain');
     res.send('test1 plain');
   });
