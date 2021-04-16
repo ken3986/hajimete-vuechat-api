41d91515bc481dfdc488195ece3be101ec02ecbf
ken3986 <naka_ken8686@yahoo.co.jp>
2021-04-17 02:58:22 +0900

    チャンネル作成、メッセージ登録、リセットメソッド導入

--- src/express.js
+++ src/express.js
@@ -11,36 +11,69 @@ app.use(cors());
 // serverless読み込み
 const serverless = require('serverless-http');
 
-const admin = require('firebase-admin');
-const serviceAccount = {
-  "type": process.env.TYPE,
-  "project_id": process.env.PROJECT_ID,
-  "private_key_id": process.env.PRIVATE_KEY_ID,
-  "private_key": process.env.PRIVATE_KEY.replace(/\\n/g, ('\n')),
-  "client_email": process.env.CLIENT_EMAIL,
-  "client_id": process.env.CLIENT_ID,
-  "auth_uri": process.env.AUTH_URI,
-  "token_uri": process.env.TOKEN_URI,
-  "auth_provider_x509_cert_url": process.env.AUTH_PROVIDER_X509_CERT_URL,
-  "client_x509_cert_url": process.env.CLIENT_X509_CERT_URL
-}
+// req.bodyから値を取得できるようにする
+const bp = require('body-parser');
+app.use(bp.json());
+app.use(bp.urlencoded({ extended: true }));
 
-if(!admin.apps.length) {
-  admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
-    databaseURL: process.env.DATABASE_URL
-  });
-}
+// Firebase RealtimeDatabaseに接続
+  const admin = require('firebase-admin');
+  const serviceAccount = {
+    "type": process.env.TYPE,
+    "project_id": process.env.PROJECT_ID,
+    "private_key_id": process.env.PRIVATE_KEY_ID,
+    "private_key": process.env.PRIVATE_KEY.replace(/\\n/g, ('\n')),
+    "client_email": process.env.CLIENT_EMAIL,
+    "client_id": process.env.CLIENT_ID,
+    "auth_uri": process.env.AUTH_URI,
+    "token_uri": process.env.TOKEN_URI,
+    "auth_provider_x509_cert_url": process.env.AUTH_PROVIDER_X509_CERT_URL,
+    "client_x509_cert_url": process.env.CLIENT_X509_CERT_URL
+  }
 
-const usersRef = admin.database().ref("users");
-const channelsRef = admin.database().ref('channels');
+  if(!admin.apps.length) {
+    admin.initializeApp({
+      credential: admin.credential.cert(serviceAccount),
+      databaseURL: process.env.DATABASE_URL
+    });
+  }
 
-const getData = (ref) => {
-  return new Promise((resolve, reject) => {
-    const onDataCallback = snapshot => resolve(snapshot.val())
-    ref.on('value', onDataCallback)
-  });
-}
+  const usersRef = admin.database().ref("users");
+  // const channelsRef = admin.database().ref('channels');
+
+  const getData = (ref) => {
+    return new Promise((resolve, reject) => {
+      const onDataCallback = snapshot => resolve(snapshot.val())
+      ref.on('value', onDataCallback)
+    });
+  }
+
+// ユーザー認証
+  const anonymousUser = {
+    id: "anon",
+    name: "Anonymous",
+    avatar: ""
+  };
+
+  const checkUser = (req, res, next) => {
+    req.user = anonymousUser;
+    if(req.query.auth_token != undefined) {
+      let idToken = req.query.auth_token(idToken).then(decodedIdToken => {
+        let authUser = {
+          id: decodedIdToken.user_id,
+          name: decodedIdToken.name,
+          avatar: decodedIdToken.picture
+        };
+        req.user = authUser;
+        next();
+      }).catch(error => {
+        next();
+      });
+    } else {
+      next();
+    }
+  }
+  app.use(checkUser);
 
 // ルーティング
 const router = express.Router();
@@ -81,6 +114,84 @@ const router = express.Router();
       res.header('Content-Type', 'application/json; charset=utf-8');
       res.send({channels: items});
     });
+  });
+
+  // チャンネルの作成関数
+  function createChannel(cname) {
+    let channelsRef = admin.database().ref('channels');
+    let date1 = new Date();
+    let date2 = new Date();
+    date2.setSeconds(date2.getSeconds() + 1);
+    const defaultData = `{
+      "messages": {
+        "1": {
+          "body": "Welcome to #${cname} channel!",
+          "date": "${date1.toJSON()}",
+          "user": {
+            "avatar": "",
+            "id": "robot",
+            "name": "Robot"
+          }
+        },
+        "2": {
+          "body": "はじめてのメッセージを投稿してみましょう。",
+          "date": "${date2.toJSON()}",
+          "user": {
+            "avatar": "",
+            "id": "robot",
+            "name": "Robot"
+          }
+        }
+      }
+    }`;
+    channelsRef.child(cname).set(JSON.parse(defaultData));
+  }
+  // チャンネルの作成
+  router.post('/channels', (req, res) => {
+    let cname = req.body.cname;
+    createChannel(cname);
+    res.header('Content-Type', 'application/json; charset=utf-8');
+    res.status(201).json({result: 'ok' + ' ' + req.body.cname});
+  });
+
+  // メッセージ一覧の取得
+  router.get('/channels/:cname/messages', (req, res) => {
+    let cname = req.params.cname;
+    let messagesRef = admin.database().ref(`channels/${cname}/messages`).limitToLast(20);
+    messagesRef.once('value', function(snapshot) {
+      let items = new Array();
+      snapshot.forEach(function(childSnapshot) {
+        let message = childSnapshot.val();
+        message.id = childSnapshot.key;
+        items.push(message);
+      });
+      items.reverse();
+      res.header('Content-Type', 'application/json; charset=utf-8');
+      res.send({messages: items});
+    });
+  });
+
+  // メッセージの追加
+  router.post('/channels/:cname/messages', (req, res) => {
+    let cname = req.params.cname;
+    let message = {
+      date: new Date().toJSON(),
+      body: req.body.body,
+      user: req.user
+    };
+    let messagesRef = admin.database().ref(`channels/${cname}/messages`);
+    messagesRef.push(message);
+    res.header('Content-Type', 'application/json; charset=utf-8');
+    res.status(201).send({result: 'ok'});
+    res.end();
+  });
+
+  // 初期状態に戻す（注：データが消える）
+  router.post('/reset', (req, res) => {
+    createChannel('general');
+    createChannel('random');
+    res.header('Content-Type', 'application/json; charset=utf8');
+    res.status(201).send({result: 'ok'});
   });
 
 app.use('/.netlify/functions/express', router);
